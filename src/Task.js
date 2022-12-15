bbf75b8cc9ec5ee8dd9275bd284c2924d233de35
adityadwn <223200009@student.unaki.ac.id>
2022-12-15 18:49:47 +0700

    add src file

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Aditya Dwi Nandra" // TODO: replace this
+const studentId = "FE2718873" // TODO: replace this
+
+export { url, studentName, studentId }
